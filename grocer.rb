8d79842c54465c2809fac05ce5f5eed3b1c01ddf
Stacey  Marks <staceyamarks@gmail.com>
2018-02-07 16:14:34 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -40,6 +40,7 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  cart_w_coupons = apply_coupons(consolidated_cart)
+  cart_w_coupons = apply_coupons(consolidated_cart, coupons)
+  
   
 end
