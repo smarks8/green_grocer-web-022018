357d5f4b104afd128ce30464a466b88ac0053bb4
Stacey  Marks <staceyamarks@gmail.com>
2018-02-07 15:38:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item_name = coupon[:item]
-    if cart[item_name] && 
+    if cart[item_name] && cart[item_name]
 end
 
 def apply_clearance(cart)
